560f6d09f7ace8664112d64c1c67ce7f51a865cc
ash14ish <ash14ishh@gmail.com>
2022-07-08 17:09:36 +0530

    pizzaart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import classes from "./Header.module.css";
+import { IoMdPizza } from "react-icons/io";
 
 const Header = () => {
   return (
     <>
       <header className={classes.header}>
-        <h2>Explore Menu</h2>
+        <h2>
+          Explore Menu <IoMdPizza className={classes["pizza-icon"]} />
+        </h2>
       </header>
     </>
   );
