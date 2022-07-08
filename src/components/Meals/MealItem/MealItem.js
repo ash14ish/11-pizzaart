0e222ff0d85e9b8ad1e8272a6e01544f7f005fde
ash14ish <ash14ishh@gmail.com>
2022-07-08 14:06:46 +0530

    pizzaart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -23,9 +23,9 @@ const MealItem = props => {
   return (
     <li className={classes.meal}>
       <div>
-        <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
+        <h4>{props.name}</h4>
+        <p className={classes.description}>{props.description}</p>
       </div>
     </li>
   );
