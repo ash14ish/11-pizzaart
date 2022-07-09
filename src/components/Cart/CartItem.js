ff01b5f3465d6405ff91ab40feca884d189162f1
ash14ish <ash14ishh@gmail.com>
2022-07-09 14:24:21 +0530

    pizzaart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -7,21 +7,27 @@ const CartItem = props => {
 
   return (
     <li className={classes["cart-item"]}>
-      <div>
+      <img src={props.image} />
+
+      <div className={classes["cart-item-description"]}>
         <h3>{props.name}</h3>
 
+        <div className={classes["actions-container"]}>
+          {props.amount !== 1 ? (
+            <TiMinus className={classes.icons} onClick={props.onRemove} />
+          ) : (
+            <RiDeleteBinLine
+              className={classes.icons}
+              onClick={props.onRemove}
+            />
+          )}
+
+          <span className={classes["item-amount"]}>{props.amount}</span>
+
+          <TiPlus className={classes.icons} onClick={props.onAdd} />
+        </div>
+
         <span className={classes.price}>{price}</span>
-      </div>
-
-      <div className={classes["actions-container"]}>
-        {props.amount !== 1 ? (
-          <TiMinus className={classes.icons} onClick={props.onRemove} />
-        ) : (
-          <RiDeleteBinLine className={classes.icons} onClick={props.onRemove} />
-        )}
-
-        <span className={classes["item-amount"]}>{props.amount}</span>
-        <TiPlus className={classes.icons} onClick={props.onAdd} />
       </div>
     </li>
   );
