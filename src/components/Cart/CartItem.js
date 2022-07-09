3ac1c31872a4016916d80894e3edd73109cd7016
ash14ish <ash14ishh@gmail.com>
2022-07-09 14:44:46 +0530

    pizzaart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -7,27 +7,31 @@ const CartItem = props => {
 
   return (
     <li className={classes["cart-item"]}>
-      <img src={props.image} />
+      <div className={classes["image-container"]}>
+        <img src={props.image} />
+      </div>
 
       <div className={classes["cart-item-description"]}>
         <h3>{props.name}</h3>
 
-        <div className={classes["actions-container"]}>
-          {props.amount !== 1 ? (
-            <TiMinus className={classes.icons} onClick={props.onRemove} />
-          ) : (
-            <RiDeleteBinLine
-              className={classes.icons}
-              onClick={props.onRemove}
-            />
-          )}
+        <div className={classes["cart-item-amount"]}>
+          <div className={classes["actions-container"]}>
+            {props.amount !== 1 ? (
+              <TiMinus className={classes.icons} onClick={props.onRemove} />
+            ) : (
+              <RiDeleteBinLine
+                className={classes.icons}
+                onClick={props.onRemove}
+              />
+            )}
 
-          <span className={classes["item-amount"]}>{props.amount}</span>
+            <span className={classes["item-amount"]}>{props.amount}</span>
 
-          <TiPlus className={classes.icons} onClick={props.onAdd} />
+            <TiPlus className={classes.icons} onClick={props.onAdd} />
+          </div>
+
+          <span className={classes.price}>{price}</span>
         </div>
-
-        <span className={classes.price}>{price}</span>
       </div>
     </li>
   );
